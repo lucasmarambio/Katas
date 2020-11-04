b559354d9b8cfdaa9d1a76a3a031528487a3d649
Lucas Marambio <lucas.e.marambio@gmail.com>
2020-11-04 02:51:22 -0300

    Add first-class-method-tostring

---
+++ OOP/first-class-method-tostring/first-class-method-tostring/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace first_class_method_tostring
+{
+    public class Person
+    {
+        private string _name;
+
+        public Person(string name)
+        {
+            _name = name;
+        }
+
+        public override string ToString()
+        {
+            return @"Hello! My name is " + _name.ToString();
+        }
+    }
+}
---
+++ OOP/first-class-method-tostring/first-class-method-tostring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace first_class_method_tostring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var amountOfPersons = 3;
+            var persons = new List<Person>();
+
+            for (int i = 0; i < amountOfPersons; i++)
+            {
+                persons.Add(new Person(Console.ReadLine()));
+            }
+
+            persons.ForEach(x => Console.WriteLine(x.ToString()));
+        }
+    }
+}
